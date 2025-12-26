0d0bcd663872b6f48afc0060c2d9720feda71dc0
Saurabh Gupta <saurabhg90055@gmail.com>
2025-12-27 03:58:31 +0530

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,14 @@
+import sys
+
+def error_meggase_detals(error, error_details:sys):
+    _, _, exc_tb = error_details.exc_info()
+    filename = exe_tb.tb_frame.f_code.co_filename
+    error_message = f"error occured in python name {0}, line number {1}, error message {2}".format(filename, exc_tb.tb_lineno, error)
+    return error_message
+class CoustomExeption(Exception):
+    def __init__(self, error_message, error_details:sys):
+        super().__init__(error_message)
+        self.error_message = error_meggase_detals(error_message, error_details)
+
+    def __str__(self):
+        return self.error_message
